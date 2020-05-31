d654271d72d7b48df05d102e4db1d7a4c44661ca
darwinys123 <45078173+darwinys123@users.noreply.github.com>
2020-06-01 06:38:02 +0800

    Updates

---
+++ precache-manifest.d647df6b29534e4f177fdb7d308e95fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ced1d8e2867a6a35bf568c84348bc0",
+    "url": "/Video-Search/index.html"
+  },
+  {
+    "revision": "5ca072997d3b5c04a521",
+    "url": "/Video-Search/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "ac25617425a754eae771",
+    "url": "/Video-Search/static/js/2.b9c5a873.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/Video-Search/static/js/2.b9c5a873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca072997d3b5c04a521",
+    "url": "/Video-Search/static/js/main.a671191e.chunk.js"
+  },
+  {
+    "revision": "ef61d9b89f622f8e109a",
+    "url": "/Video-Search/static/js/runtime-main.c8aa4c87.js"
+  }
+]);
